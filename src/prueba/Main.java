396f7cbffa3987e1b4cb0c5f8d88cb1c347ed379
Allan19Prieto <allanbanilla@gmail.com>
2021-10-07 21:39:24 -0600

    Comentarios

--- src/prueba/Main.java
+++ src/prueba/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
+        //Titulo y mandamos la nueva escena
         primaryStage.setTitle("P2P Game");
         new WelcomeController(primaryStage).displayScene();
     }
